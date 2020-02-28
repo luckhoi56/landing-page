7d2c20406778588c01e66c46557cc51859959306
Eric Luc <ericluc@Erics-MacBook-Pro.local>
2020-02-27 20:05:59 -0500

    Subject: added the hightlighted section when scrolled

--- js/app.js
+++ js/app.js
@@ -20,6 +20,7 @@
 */
 var fragment = document.createDocumentFragment();
 
+
 /**
  * End Global Variables
  * Start Helper Functions
@@ -39,7 +40,7 @@ function navBuild(){
     var fragment = document.createDocumentFragment();
 
     const navBar = document.querySelector('#navbar__list');
-    for(let i = 1; i <=3; i++){
+    for(let i = 3; i >= 3; i--){
         let m_dom = document.createElement('li')
         let m_string = `<a class ="section_${i}" href="#section${i}">Section ${i}</a>`;
         m_dom.innerHTML=m_string;
@@ -51,7 +52,6 @@ function navBuild(){
 
 // Add class 'active' to section when near top of viewport
 
-
 // Scroll to anchor ID using scrollTO event
 
 
@@ -62,9 +62,34 @@ function navBuild(){
 */
 
 // Build menu 
+navBuild();
+window.onscroll = function(){add_active()};
 
 // Scroll to section on link click
 
 // Set sections as active
 
-navBuild();
+function initiate_variable(){
+   
+
+}
+function add_active(){
+    let section1 = document.getElementsByClassName("section_1")[0];
+    let section2 = document.getElementsByClassName("section_2")[0];
+    let section3 = document.getElementsByClassName("section_3")[0];
+    if (window.scrollY >=471 && window.scrollY <800){
+        section1.style.backgroundColor ="green";
+        section2.style.backgroundColor ="transparent";
+        section3.style.backgroundColor ="transparent";
+    }
+    else if (window.scrollY >=1042 && window.scrollY <1500){
+        section1.style.backgroundColor ="transparent";
+        section2.style.backgroundColor ="green";
+        section3.style.backgroundColor ="transparent";
+    }
+    else if (window.scrollY >=1700){
+        section1.style.backgroundColor ="transparent";
+        section2.style.backgroundColor ="transparent";
+        section3.style.backgroundColor ="green";
+    }
+}
